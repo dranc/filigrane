0be584b808fa759cb9fe466c8ac0d67efa7ebf97
dranc <dimitri.ranc@ubisoft.com>
2023-11-23 17:51:16 +0100

    add some change

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,56 @@
-import { FiligraneServices } from "./filigrane.services";
+import './index.scss';
+
+import { FiligraneServices } from './filigrane.services';
 
 (() => {
-    const filesMap = new Map<string, File>();
+  const filesMap = new Map<string, File>();
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const inputFile = document.getElementById('input-file') as HTMLInputElement;
-        const inputText = document.getElementById('input-text') as HTMLInputElement;
-        const submit = document.getElementById('submit');
+  document.addEventListener('DOMContentLoaded', () => {
+    const inputFile = document.getElementById('input-file') as HTMLInputElement;
+    const inputText = document.getElementById('input-text') as HTMLInputElement;
+    const submit = document.getElementById('submit');
 
-        const files = document.getElementById('files')!;
+    const files = document.getElementById('files')!;
+    const fileExample = files.children[0];
 
-        inputFile.addEventListener('change', (ev) => {
-            for(const file of inputFile.files!) {
-                const div = document.createElement('div');
-                div.id = crypto.randomUUID();
-                div.classList.add('file');
+    inputFile.addEventListener('change', (ev) => {
+      for (const file of inputFile.files!) {
+        const id = crypto.randomUUID();
 
-                const checkbox = document.createElement('input');
-                checkbox.id = 'checkbox';
-                checkbox.type = 'checkbox';
+        console.log(fileExample);
 
-                const img = document.createElement('img');
-                img.setAttribute('src', 'icons/icon_16.png');
+        const newFileElement = fileExample.cloneNode(true) as HTMLElement;
+        newFileElement.id = id;
 
-                const name = document.createElement('label');
-                name.innerText = file.name;
+        (newFileElement.getElementsByClassName('name')[0] as HTMLElement).innerText = file.name;
+        (newFileElement.getElementsByClassName('size')[0] as HTMLElement).innerText = file.size.toString();
 
-                const size = document.createElement('label');
-                size.innerText = file.size.toString();              
-
-                div.append(checkbox, img, name, size);
-
-                files.replaceChildren(div);
-                filesMap.set(div.id, file);
-            }
-        });
-
-        submit!.onclick = (async (ev) => {
-            if (inputText.value && files.children.length > 0) {                
-                for(const fileElt of files.children) {
-                    const file = filesMap.get(fileElt.id);
-                    if (!file) {
-                        throw Error('File lost ?')
-                    }
-                    const newFile = await FiligraneServices.addFiligraneToFile(file, inputText.value);
-                    const seePreview = document.createElement('button');
-                    seePreview.innerText='see preview';
-                    seePreview.onclick = () => {
-                        console.log(newFile.name)
-                        const t = new Blob([newFile], { type: 'application/pdf' });
-                        window.open(URL.createObjectURL(t))
-                    }
-                    fileElt.appendChild(seePreview);
-                }
-            }
-        });
+        files.appendChild(newFileElement);
+        filesMap.set(id, file);
+      }
     });
+
+    submit!.onclick = async (ev) => {
+      if (inputText.value && files.children.length > 0) {
+        for (const fileElt of files.children) {
+          if (fileElt.id && (fileElt.getElementsByClassName('selected')[0] as HTMLInputElement).checked) {
+            console.log(`sending ${fileElt.id}`);
+            const file = filesMap.get(fileElt.id);
+            if (!file) {
+              throw Error(`File ${fileElt.id} lost ?`);
+            }
+            const newFile = await FiligraneServices.addFiligraneToFile(file, inputText.value);
+            const seePreview = document.createElement('button');
+            seePreview.innerText = 'see preview';
+            seePreview.onclick = () => {
+              console.log(newFile.name);
+              const t = new Blob([newFile], { type: 'application/pdf' });
+              window.open(URL.createObjectURL(t));
+            };
+            fileElt.appendChild(seePreview);
+          }
+        }
+      }
+    };
+  });
 })();
